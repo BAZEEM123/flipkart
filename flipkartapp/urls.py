14df19e021d33ec2588114f9d411365ee553ac1f
BAZEEM123 <155583917+BAZEEM123@users.noreply.github.com>
2024-02-23 20:09:45 +0530

    fl
    ip

---
+++ flipkartapp/urls.py
@@ -0,0 +1,9 @@
+from .import views
+from django.urls import path
+app_name='flipkartapp'
+urlpatterns=[
+
+    path('',views.allproductCat,name='allproductCat'),
+    path('<slug:c_slug>/',views.allproductCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views. proDetail,name='prodCatdetail')
+]
